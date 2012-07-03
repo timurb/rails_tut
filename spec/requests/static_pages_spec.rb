c4eeb69bc51b9742c33fc62df03c42430b34b19c
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-07-04 00:29:14 +0400

    add checking for base title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,11 +10,13 @@ def page(path,header,title=nil)
   it "should have the title '#{title}'" do
     visit "/static_pages/#{path}"
     page.should have_selector('title',
-             :text => "| #{title}")
+             :text => "#{base_title} | #{title}")
   end
 end
 
 describe "Static Pages" do
+
+  let (:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
   describe "Home Page"  do
     page('home', 'Sample App', 'Home')
