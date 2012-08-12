cdd0b10b1ff8b9a7a4312d96925680b23d327d72
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-08-12 11:50:24 +0400

    feed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -171,12 +171,24 @@ describe User do
 
     describe "status" do
       let(:unfollowed_post) do
-        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user_sequence))
+      end
+
+      let(:followed_user) { FactoryGirl.create(:user_sequence) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 
